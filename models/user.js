4bbc3aa259068327a1c7d71bd39ddc3c7f9f5569
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-28 09:08:19 -0700

    added user schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true }
+});
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function(password) {
+    return bcrypt.hash(password, 10);
+};  
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
