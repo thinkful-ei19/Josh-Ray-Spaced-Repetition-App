03c78ab5f367d5ba0949dd9ad7e3a328f0643e22
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-30 07:56:44 -0700

    modified router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,20 +5,47 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 
 const User = require('../models/user');
+const Queue = require('../queue/queue-class');
 
 router.use(bodyParser.json());
 
+const questionQueue = new Queue();
 
-// GET USER QUESTIONS
+// GET ALL USER QUESTIONS
+// router.get('/questions', (req, res, next) => {
+//   // console.log(req.user);
+//   User.findById(req.user.id)
+//     .populate('questions')
+//     .then(user => {
+//       console.log(user.questions);
+//       res.json(user.questions);
+//     })
+// });
+
+function peek(queue) {
+  let currNode = queue.first;
+  let displayQueue = currNode.value;
+  return displayQueue;
+};
+
+
+// GET FIRST QUESTION IN QUEUE
 router.get('/questions', (req, res, next) => {
   // console.log(req.user);
   User.findById(req.user.id)
     .populate('questions')
     .then(user => {
-      console.log(user);
-      res.json(user.questions);
+      // console.log(user.questions);
+      user.questions.map(question => questionQueue.enqueue(question));
+      // console.log(questionQueue);
+      res.json(peek(questionQueue));
     })
 });
 
+
+// router.put('/questions', (req, res, next) => {
+//   User.findById(req.user.id)
+    
+// });
 
 module.exports = router;
