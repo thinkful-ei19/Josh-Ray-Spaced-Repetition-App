e2b134624fa47ff4df5466ef74b9980b55639874
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-30 17:00:09 -0700

    edited database_url

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://hutch:josh@ds237660.mlab.com:37660/learn-dothraki-users',
+        process.env.DATABASE_URL || 'mongodb://Ray-Yeung:carter88@ds137720.mlab.com:37720/josh-ray-spaced-repitition-app',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
